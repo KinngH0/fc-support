e20458d93bbe60b539b371c913412c60d54f82ef
Gyeonghwan Kim(김경환)[Mold Repair Group] <kh.kim@slworld.com>
2025-04-30 10:23:09 +0900

    Simplify to most basic Flask setup

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello from Flask!"
-
-@app.route("/healthz")
-def healthz():
-    return jsonify({"status": "ok"}) 
+@app.route('/')
+def hello():
+    return 'Hello World!' 
