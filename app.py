22efca2118a509dc40e317a7f54c714249894ae0
Gyeonghwan Kim(김경환)[Mold Repair Group] <kh.kim@slworld.com>
2025-04-30 10:29:02 +0900

    Initialize basic Flask application structure

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route("/")
+def home():
+    return "Hello, Render!"
 
 if __name__ == '__main__':
     app.run(debug=True) 
