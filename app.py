e46bda5b31a2e43bd07cae1154dad3ef80a317a3
KinngHO <rudghks4779@gmail.com>
2025-05-03 18:37:56 +0900

    Add React build files and update Flask configuration

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+@app.route('/')
+def serve():
+    return send_from_directory('static', 'index.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000) 
