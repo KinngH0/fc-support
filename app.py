94d0e862b17b4c2fc58ecaf740ce26d8da16f46a
KinngHO <rudghks4779@gmail.com>
2025-05-03 19:04:23 +0900

    Add logo and favicon

--- app.py
+++ app.py
@@ -1,25 +1,14 @@
-from flask import Flask, send_from_directory, send_file
-import os
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='build', static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_file('favicon.ico')
-
-@app.route('/<path:path>')
-def serve_static(path):
-    if os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
-    return send_from_directory(app.static_folder, 'index.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/healthz')
 def health_check():
-    return 'OK', 200
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000) 
